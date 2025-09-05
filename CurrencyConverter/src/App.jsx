da55f85cbe7ec43cff6785cffe7e0f479846b42d
darshana.gohil <darshana.gohil@silvertouch.com>
2025-09-05 18:36:35 +0530

    created CurrencyConverter project

---
+++ CurrencyConverter/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-2xl font-bold text-amber-300'>Vite + React</h1>
+    </>
+  )
+}
+
+export default App
